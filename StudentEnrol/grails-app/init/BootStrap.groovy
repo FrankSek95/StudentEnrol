c4d0fbfd6d81f228eee2c098f3fc9bb532eef656
todza66 <t.d.maswera@gmail.com>
2016-02-02 12:01:03 +0000

    Tested the class Course, Implemented scaffolding and bootstrapping

--- StudentEnrol/grails-app/init/BootStrap.groovy
+++ StudentEnrol/grails-app/init/BootStrap.groovy
@@ -1,6 +1,33 @@
-class BootStrap {
+import com.studentenrol.*
+
+class BootStrap{
 
     def init = { servletContext ->
+
+	def course1=new Course( title: 'Interactive Media with Animation',
+				department: 'Computing',
+				description: '''Lorem ipsum dolor sit amet, ad sea solum brute sensibus, te summo senserit vix, mel reque corpora vulputate ei.''',
+				leader: 'Joe Jojo',
+				code: 'IMA101',
+				startDate: new Date('19/01/2015'),
+				endDate: new Date('19/01/2020')).save()
+
+def course2=new Course( title: 'Databases',
+				department: 'Computing',
+				description: '''SQL databases + Oracle''',
+				leader: 'Will Smith',
+				code: 'D101',
+				startDate: new Date('19/01/2015'),
+				endDate: new Date('19/01/2020')).save()
+
+def course3=new Course( title: 'PCP',
+				department: 'Computing',
+				description: '''Employability''',
+				leader: 'Shabaz Khalid',
+				code: 'PCP101',
+				startDate: new Date('19/01/2015'),
+				endDate: new Date('19/01/2020')).save()
+
     }
     def destroy = {
     }
